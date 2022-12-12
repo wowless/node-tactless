2f1593349f53e60d72d5c608adee52e36f777428
Ferronn <ferronn@ferronn.dev>
2022-12-12 19:54:04 +0000

    fix index export

--- tactless.js
+++ tactless.js
@@ -4,6 +4,6 @@ const pipe = require('./pipe');
 
 module.exports = {
   config: s => config.parse(s),
-  index: s => index.parse(s),
+  index: (s, n) => index.parse(s, n),
   pipe: s => pipe.parse(s),
 };
